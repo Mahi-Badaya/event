5c4a2d722007085ee4b237d1066c07b55f20e0d2
Mahi-Badaya <badayamahi11@gmail.com>
2022-04-10 01:48:50 +0530

    added checkin button

--- lib/Components/constants.dart
+++ lib/Components/constants.dart
@@ -5,7 +5,7 @@ var url = 'https://blogmedia.evbstatic.com/wp-content/uploads/wpmulti/sites/8/20
 
 String randomText = 'Lorem ipsum dolor sit amet. Eos distinctio quaerat qui quia quia et similique facilis non voluptate reiciendis. Eos unde sequi nam sint consequatur eos libero recusandae rem dolor asperiores et magnam necessitatibus expedita architecto.Et modi pariatur qui autem nobis 33 dolor repellendus. In fugit nobis At labore delectus ut maxime rerum est omnis autem qui molestias dicta nam commodi ipsa aut doloribus voluptas. Qui odit galisum sed reiciendis magni id corrupti necessitatibus qui corrupti harum et eveniet corrupti!Ad consectetur necessitatibus et quia commodi in illum voluptatem sed consequatur fugit non deserunt voluptate. Rem repellendus eaque est provident autem nam iure. Ex nesciunt numquam a quasi autem hic esse ipsam est quis molestiae in dolorem eligendi sed fuga adipisci qui ipsa omnis. Repellat officiis ex repudiandae voluptatum qui mollitia ullam.';
 
-Widget description (String text) {
+Widget Description (String text) {
  final lines = isShowmore ? null :4;
  return Text(
      text,
--- lib/Screens/event_details.dart
+++ lib/Screens/event_details.dart
@@ -8,6 +8,7 @@ class EventDetails extends StatefulWidget {
   @override
   State<EventDetails> createState() => _EventDetailsState();
 }
+
 bool isShowmore = false;
 
 class _EventDetailsState extends State<EventDetails> {
@@ -29,90 +30,119 @@ class _EventDetailsState extends State<EventDetails> {
         elevation: 0,
         backgroundColor: Colors.green,
       ),
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                height: 250,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  image: DecorationImage(
-                    image: NetworkImage(url),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.5),
-                      BlendMode.darken,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
+                  height: 250,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    image: DecorationImage(
+                      image: NetworkImage(url),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5),
+                        BlendMode.darken,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 30.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:  [
-                    const Text(
-                      'Lorem Ipsum',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24.0,
-                          color: Colors.white),
-                    ),
-                    SizedBox(height: 7.0,),
-                    Text(
-                      randomText,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 30.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Lorem Ipsum',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 7.0,
+                      ),
+                      Text(
+                        randomText,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
                           fontWeight: FontWeight.w400,
                           fontSize: 16.0,
                           color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Lorem Ipsum',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24.0,
+                            ),
+                          ),
+                          Text('10:30 am - 1:15 pm | COEP | Pune',
+                              style: TextStyle(color: Colors.grey)),
+                        ],
+                      ),
+                      const CustomIcon(
+                        circularIcon: Icons.share_outlined,
+                        colour: Colors.green,
+                      ),
+                      const CustomIcon(
+                        circularIcon: Icons.whatsapp_outlined,
+                        colour: Colors.blue,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Description(randomText),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isShowmore = !isShowmore;
+                      });
+                    },
+                    child: Text((isShowmore ? 'Show less' : 'Show more')),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green[400],
+                      padding: EdgeInsets.symmetric(horizontal: 140, vertical:18.0),
+                      textStyle: TextStyle(fontSize: 19),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
                       ),
                     ),
-                  ],
-                ),
+                    onPressed: () {},
+                    child: Text('Check In',style: TextStyle(letterSpacing: 2),),
+                  ),
+                ],
               ),
-            ],
-          ),
-
-             Row(
-               mainAxisAlignment: MainAxisAlignment.spaceAround,
-               children: [
-                 Column(
-                   mainAxisAlignment: MainAxisAlignment.start,
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: const [
-                     Text(
-                      'Lorem Ipsum',
-                        style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24.0,),),
-                     Text('10:30 am - 1:15 pm | COEP | Pune',style: TextStyle(color: Colors.grey)),
-                   ],
-                 ),
-                 const CustomIcon(
-                   circularIcon: Icons.share_outlined,
-                   colour: Colors.green,
-                 ),
-                 const CustomIcon(
-                   circularIcon: Icons.whatsapp_outlined,
-                   colour: Colors.blue,
-                 ),
-               ],
-             ),
-          description(randomText),
-          TextButton(onPressed: (){
-            setState(() {
-              isShowmore = !isShowmore;
-            });
-          }, child: Text((isShowmore? 'Show less': 'Show more')))
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
